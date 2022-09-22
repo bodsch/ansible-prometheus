fcb0f495016837a13c43fb7cc49bf7da752f70aa
Bodo Schulz <bodo@boone-schulz.de>
2022-09-22 09:04:19 +0200

    fix alerting
    work on config validatator

--- filter_plugins/prometheus.py
+++ filter_plugins/prometheus.py
@@ -19,6 +19,7 @@ class FilterModule(object):
         return {
             'validate_file_sd': self.validate_file_sd,
             'prometheus_checksum': self.checksum,
+            'validate_alertmanagers': self.validate_alertmanagers,
         }
 
     def validate_file_sd(self, data, targets):
@@ -73,3 +74,65 @@ class FilterModule(object):
         # display.v("= checksum: {}".format(checksum))
 
         return checksum
+
+    def validate_alertmanagers(self, data):
+        """
+        """
+        display.v(f"validate_alertmanagers({data})")
+
+        supported = ["static_configs"]
+        known_sd_configs = [
+            "azure_sd_configs", "consul_sd_configs","dns_sd_configs","ec2_sd_configs", "eureka_sd_configs", "file_sd_configs",
+               "digitalocean_sd_configs", "docker_sd_configs",  "dockerswarm_sd_configs", "gce_sd_configs", "hetzner_sd_configs",
+               "http_sd_configs",  "kubernetes_sd_configs", "lightsail_sd_configs","linode_sd_configs", "marathon_sd_configs",
+"nerve_sd_configs", "nerve_sd_configs", "openstack_sd_configs", "puppetdb_sd_configs", "scaleway_sd_configs", "serverset_sd_configs",
+"triton_sd_configs","uyuni_sd_configs", "static_configs",
+                      ]
+        present = []
+
+        if isinstance(data, list):
+            for d in data:
+                keys = d.keys()
+                display.v(f"  - name: {d}")
+                display.v(f"  - name: {keys}")
+
+                if keys in known_sd_configs:
+                    azure_sd = d.get("azure_sd_configs", None)
+                    consul_sd = d.get("consul_sd_configs", None)
+                    dns_sd = d.get("dns_sd_configs", None)
+                    ec2_sd = d.get("ec2_sd_configs", None)
+                    eureka_sd = d.get("eureka_sd_configs", None)
+                    file_sd = d.get("file_sd_configs", None)
+                    digitalocean_sd = d.get("digitalocean_sd_configs", None)
+                    docker_sd = d.get("docker_sd_configs", None)
+                    dockerswarm_sd = d.get("dockerswarm_sd_configs", None)
+                    gce_sd = d.get("gce_sd_configs", None)
+                    hetzner_sd = d.get("hetzner_sd_configs", None)
+                    http_sd = d.get("http_sd_configs", None)
+                    kubernetes_sd = d.get("kubernetes_sd_configs", None)
+                    lightsail_sd = d.get("lightsail_sd_configs", None)
+                    linode_sd = d.get("linode_sd_configs", None)
+                    marathon_sd = d.get("marathon_sd_configs", None)
+                    nerve_sd = d.get("nerve_sd_configs", None)
+                    openstack_sd = d.get("openstack_sd_configs", None)
+                    puppetdb_sd = d.get("puppetdb_sd_configs", None)
+                    scaleway_sd = d.get("scaleway_sd_configs", None)
+                    serverset_sd = d.get("serverset_sd_configs", None)
+                    triton_sd = d.get("triton_sd_configs", None)
+                    uyuni_sd = d.get("uyuni_sd_configs", None)
+                    static_configs = d.get("static_configs", None)
+
+            display.v(f"  - name: {keys}")
+
+            if azure_sd:
+                present.append("azure")
+            if consul_sd:
+                present.append("consul")
+            if dns_sd:
+                present.append("dns")
+            if static_configs:
+                present.append("static_configs")
+
+
+
+        return []
